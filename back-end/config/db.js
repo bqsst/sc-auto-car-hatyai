a47510ddb02d1c73fe03c56c8af7ed7061580317
Abbas Leenud <152507598+bqsst@users.noreply.github.com>
2024-11-09 23:54:36 +0700

    init dev branch

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async() => {
     try{
         await mongoose.connect('mongodb://localhost:27017/sc-auto-haiyai')
-        console.log('Database connected')
+        console.log('Database connected jaaa')
     }catch(err){
         console.log('Cannot connect to database')
     }
